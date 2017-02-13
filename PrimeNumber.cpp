49a7b83ab53fbeba7c4dade036ff6e995210de91
zakuz5 <j.zoljargal@must.edu.mn>
2017-02-13 23:18:39 +0800

    Update PrimeNumber.cpp
    Ажиллах хурдыг 1 дахин нэмэгдүүлэв.

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -1,8 +1,8 @@
 boolean is_prime(int a){
   int k=0;
-  for(int i=1; i<=a; i++){
+  for(int i=2; i<a/2; i++){
     if(a%i == 0) k++;
   }
-  if(k==2) return True;
+  if(k==0) return True;
   else return False;
 }
